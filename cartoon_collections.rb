62529250966be30687c2197841b34435be8de32c
John Stassen <john.stassen@gmail.com>
2020-08-22 02:55:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 def find_the_cheese(list)# code an argument here
   list.each do |food|
     cheese_types = ["cheddar", "gouda", "camembert"]
-    if cheese_types.include(food)
+    if cheese_types.include?(food)
       return food
     end
   end
